1451ae1641604fb560024b529242d81c660096ab
hnboy <hnywolf@gmail.com>
2017-04-04 22:47:12 +0800

    fixed some bug

--- google.py
+++ google.py
@@ -3,15 +3,16 @@ import requests
 import urllib.request
 from HandleJs import Py4Js
 
-def is_chinese(uchar):
+def isChinese(uchar):
     if uchar >=u'\u4e00' and uchar<=u'\u9fa5':
         return 'True'
     else:
         return 'False'
 
-def translate(context):
-    s=requests.Session();
-    if(is_chinese(context)=='True'):
+def Translate(context):
+
+    s=requests.Session()
+    if(isChinese(context)=='True'):
         targe_lang="en"
     else:
         targe_lang="zh-cn"
@@ -22,6 +23,7 @@ def translate(context):
     url="http://translate.google.cn/translate_a/single?client=t&sl=auto&tl=%s&hl=en&" \
         "dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ie=UTF-8&oe=UTF-8&otf=1&" \
         "ssel=3&tsel=3&kc=3&tk=%s&q=%s"%(targe_lang,tk,context)
+
     headers = {
                 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
                 'Accept-Encoding':'gzip, deflate, sdch',
@@ -32,20 +34,18 @@ def translate(context):
                 'Upgrade-Insecure-Requests':'1',
                 'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2987.133 Mobile Safari/537.36'
         }
+
     r = s.get(url,headers=headers)
     s.close()
     if(targe_lang=="zh-cn"):
         out=""
         result = r.text.split('"')
         loop = len(result)
-        def is_chinese1(uchar):
-            for i in uchar :
-                if i>=u'\u4e00' and i<=u'\u9fa5':
-                   return True
+
         for a in range(1,int(loop/2-2)):
             if(result[a]=="en"):
                 break
-            if(is_chinese1(result[a])):
+            if(isChinese(result[a])=='True'):
                 out+=result[a]
         return out
     else:
@@ -53,18 +53,17 @@ def translate(context):
         result = result.lstrip('[]')
         return result
 
-def saveresult(context):
+def saveResult(context):
     with open("result.txt",'a+') as f:
         f.writelines(context)
     f.close()
 
-def filetranslate(filename):
-    context=""
+def fileTranslate(filename):
     #  with open(filename,'r',encoding='utf-8',errors='ignore') as f:
     try:
         f=open(filename,'r',encoding='utf-8',errors='ignore')
     except IOError:
-        print("fail open the file, please check the filename")
+        print("open failed, please check the filename")
     else:
         f.readline()
         for line in f:
@@ -72,15 +71,15 @@ def filetranslate(filename):
             if not len(line):
                 continue
             else:
-                a = translate(line)
-                saveresult(a)
+                a = Translate(line)
+                saveResult(a)
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     trans = input("if you want to translate file input 1 or input 2 :")
     if(trans=="1"):
         filename = input("please input filename:")
-        filetranslate(filename)
+        fileTranslate(filename)
     else:
-        context=input("please input the context:")
-        a = translate(context)
+        context=input("please input the text:")
+        a = Translate(context)
         print(a)
